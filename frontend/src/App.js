b9fd9b7571c4def3d4cd6850ff6f78b357029d27
unknown <alex.pinaida@trade.ec>
2021-04-18 12:01:20 -0500

    feat: Added abut, navbar, and users components in front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { About } from './components/About';
+import { Navbar } from './components/Navbar';
+import { Users } from './components/Users';
 
 function App() {
   return (
     <Router>
-      <div>
+      <Navbar/>
+      <div className="container p-4">
         <Switch>
-          <Route path="/about" component={}/>
+          <Route path="/about" component={About}/>
+          <Route path="/" component={Users}/>
         </Switch>
       </div>
     </Router>
