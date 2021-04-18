632fb3fd86a8a0cd62add2685e4f2dd64c093e51
unknown <alex.pinaida@trade.ec>
2021-04-18 02:00:44 -0500

    feat: Added frontend project

--- backend/src/app.py
+++ backend/src/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config['MONGO_URI']='mongodb://localhost/pythonreactdb'
 mongo = PyMongo(app)
 
+CORS(app)
+
 db = mongo.db.users
 
 @app.route('/users', methods=['POST'])
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about" component={}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
